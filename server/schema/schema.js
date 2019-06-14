c071410ea1c79b974aa8769c19848324b706d127
Nurul Zaman <nurul.zaman@servicerocket.com>
2019-06-14 17:12:11 +0800

    [WITH NOTES] Implement has_many, many_to_one relationship between Book and Author.

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -10,14 +10,16 @@ const {
     GraphQLString, 
     GraphQLSchema,
     GraphQLID,
-    GraphQLInt
+    GraphQLInt,
+    GraphQLList
 } = graphql;
 
 // todo: Dummmy data. To be removed later
+// Book data here must have the author foreign key to demonstrate the book-to-author relationship
 const bookList = [
-    {id: '1', name: 'The Alchemist', genre: 'Fantasy'},
-    {id: '2', name: 'Harry Potter', genre: 'Thriller'},
-    {id: '3', name: 'The Alchemist', genre: 'Fiction'}
+    {id: '1', name: 'The Faraway Tree', genre: 'Fantasy', authorId: '1'},
+    {id: '2', name: 'Harry Potter', genre: 'Thriller', authorId: '3'},
+    {id: '3', name: 'The Alchemist', genre: 'Fiction', authorId: '3'}
 ];
 
 const authorList = [
@@ -26,27 +28,52 @@ const authorList = [
     {id: '3', name: 'Paul Coelho', age: '44'}
 ];
 
-// Schema has Book and Author
 // Create schema for Book
+// Book belongs to an author
 const BookType = new GraphQLObjectType({
     // i) Declare table name. Name is important as it will be used as a reference
     name: 'Book',
-    // ii) Declare field names using function. Perhaps, these are the fields that we are exposing?
+    // ii) Declare field names using FUNCTION instead of object, {}.
+    // This is related to how code is being read top-to-bottom, hence, throwing "undefined" error because BookType and AuthorType are declaration are interdependent
+    // It's a different story if only one depends on the other
+    // Using function declaration, it waits until file has been loaded before executing the code. 
+    // By doing this, all object needed will already be defined when the program executes.
     fields: () => ({
         //GraphQLID doesn't affect the object's type; it's just for GraphQL query to be flexible enough to be either 'string' or id (integer)
         id: {type: GraphQLID},
         name: {type: GraphQLString},
-        genre: {type: GraphQLString}
+        genre: {type: GraphQLString},
+        // Declare object that's related to Book and its custom type
+        // and the function to resolve the query
+        author: {
+            type: AuthorType,
+            resolve(book, args) {
+                // console.log('Parent object: ' + parent);
+                // console.log('Args object: ' + args);
+                return _.find(authorList, {id: book.authorId});
+            }
+        }
     })
 });
 
 // Create schema for Author
+// Author has many books
 const AuthorType = new GraphQLObjectType({
     name: 'Author',
     fields: () => ({
         id: {type: GraphQLID},
         name: {type: GraphQLString},
-        age: {type: GraphQLInt}
+        age: {type: GraphQLInt},
+        books: {
+            // GraphQLList because it's a list of books
+            // need to instantiate the GraphQLList object
+            type: new GraphQLList(BookType),
+            resolve(author, args) {
+                // console.log(args);
+                // Find in bookList any book with authorId that match the author id
+                return _.filter(bookList, {authorId: author.id});
+            }
+        }
     })
 });
 
